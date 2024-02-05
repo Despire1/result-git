bd8b91102d14b2085357087add20c9907b748ba3
Артем Политанский <artempolitanskij@MacBook-Air-Artem.local>
2024-02-05 11:39:59 +0300

    add date without jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import date from './index.js';
+import { createElement } from 'react';
+
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload 2.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div>{date}</div>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{className: 'App'},
+		createElement(
+			'header',
+			{className: 'App-header'},
+			createElement('img', {src: logo, className: 'App-logo', alt: 'logo'}),
+			createElement('p', null,
+			'Edit ',
+			createElement('code', null, 'src/App.js'),
+			' and save to reload 2.'
+			),
+			createElement(
+				'a',
+				{className: 'App-link', href: 'https://reactjs.org', target: "_blank", rel: "noopener noreferrer" },
+				'Learn React'
+			),
+			createElement(
+				'div',
+				null,
+				date
+			)
+		)
 	);
 }
 
